012f2977241595cef0e7e91a503ec9be725f3025
CicelyR <robinsoncicely@gmail.com>
2019-11-30 10:20:18 +0000

    Fix structure of project

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(
-    name='ECHRscraping',
+    name='echr_scraping',
     version='0.1dev',
     packages=['echr_scraping',],
     long_description=open('README.md').read(),
