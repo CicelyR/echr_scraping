8d501370c4eb1395e057977cdc38ad6718470636
CicelyR <robinsoncicely@gmail.com>
2019-11-30 10:18:30 +0000

    Fix typo in setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='ECHRscraping',
     version='0.1dev',
     packages=['echr_scraping',],
-    long_description=open('README.txt').read(),
+    long_description=open('README.md').read(),
 )
